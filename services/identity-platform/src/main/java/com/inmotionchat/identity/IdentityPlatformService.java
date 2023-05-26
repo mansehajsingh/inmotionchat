68bdc3d0516a5613c4d342f91fa17a9f2b1f8577
Mansehaj Singh <msehajs@gmail.com>
2023-05-26 09:56:31 -0600

    Added custom logback configuration to write app specific logs to file.

--- services/identity-platform/src/main/java/com/inmotionchat/identity/IdentityPlatformService.java
+++ services/identity-platform/src/main/java/com/inmotionchat/identity/IdentityPlatformService.java
@@ -1,13 +1,24 @@
 package com.inmotionchat.identity;
 
 import com.inmotionchat.core.soa.InMotionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class IdentityPlatformService extends InMotionService {
 
+    private static Logger log = LoggerFactory.getLogger(IdentityPlatformService.class);
+
     public String getServiceName() {
         return "IdentityPlatform";
+    }
+
+    @Override
+    public void awaken() {
+        log.info("Starting " + getServiceName() + " service.");
+        this.started = true;
+        log.info("Started " + getServiceName() + " service.");
     }
 
 }
