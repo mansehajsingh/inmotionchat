775390eae182942f7de1b16557181060262885a8
Mansehaj Singh <msehajs@gmail.com>
2023-05-26 16:25:06 -0600

    Added ConflictException.

---
+++ core/src/main/java/com/inmotionchat/core/exceptions/ConflictException.java
@@ -0,0 +1,9 @@
+package com.inmotionchat.core.exceptions;
+
+public class ConflictException extends InMotionException {
+
+    public ConflictException(String message) {
+        super(message);
+    }
+
+}
