57803da91c022bd91cc894ee51adad6ea1f28232
Mansehaj Singh <msehajs@gmail.com>
2023-06-01 18:14:52 -0600

    Created Role domain interface with SQLRole implementation.

---
+++ core/src/main/java/com/inmotionchat/core/data/postgres/SQLRole.java
@@ -0,0 +1,91 @@
+package com.inmotionchat.core.data.postgres;
+
+import com.inmotionchat.core.data.Schema;
+import com.inmotionchat.core.domains.Organization;
+import com.inmotionchat.core.domains.Role;
+import com.inmotionchat.core.exceptions.DomainInvalidException;
+import com.inmotionchat.core.util.validation.AbstractRule;
+import com.inmotionchat.core.util.validation.StringRule;
+import com.inmotionchat.core.util.validation.Violation;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "roles", schema = Schema.OrganizationManager)
+public class SQLRole extends AbstractDomain<Role> implements Role {
+
+    @ManyToOne
+    private SQLOrganization organization;
+
+    private String name;
+
+    private boolean isDefault = false;
+    public SQLRole() {}
+
+    public SQLRole(String name, Organization organization) {
+        this.name = name;
+        this.organization = AbstractDomain.forId(SQLOrganization.class, organization.getId());
+    }
+
+    public SQLRole(String name, Organization organization, boolean isDefault) {
+        this(name, organization);
+        this.isDefault = isDefault;
+    }
+
+    @Override
+    public Organization getOrganization() {
+        return organization;
+    }
+
+    @Override
+    public void setOrganization(Organization organization) {
+        this.organization = SQLOrganization.fromId(organization.getId());
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean isDefault() {
+        return isDefault;
+    }
+
+    @Override
+    public void setIsDefault(boolean isDefault) {
+        this.isDefault = isDefault;
+    }
+
+    @Override
+    public void validate() throws DomainInvalidException {
+        AbstractRule<String> nameRule = StringRule.forField("name")
+                .isNotNull().isNotEmpty();
+
+        List<Violation> violations = new ArrayList<>();
+        violations.addAll(nameRule.collectViolations(this.name));
+
+        if (violations.isEmpty())
+            throw new DomainInvalidException(violations);
+    }
+
+    @Override
+    public SQLRole copy() {
+        SQLRole copy = new SQLRole();
+        copy.setId(this.id);
+        copy.setName(this.name);
+        copy.setOrganization(this.organization);
+        copy.setMetadata(this.metadata());
+        return copy;
+    }
+
+}
---
+++ core/src/main/java/com/inmotionchat/core/domains/Role.java
@@ -0,0 +1,17 @@
+package com.inmotionchat.core.domains;
+
+public interface Role extends Domain<Role> {
+
+    Organization getOrganization();
+
+    void setOrganization(Organization organization);
+
+    String getName();
+
+    void setName(String name);
+
+    boolean isDefault();
+
+    void setIsDefault(boolean isDefault);
+
+}
