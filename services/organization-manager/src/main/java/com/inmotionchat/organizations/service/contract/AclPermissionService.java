37a2e99d399c23b54d2910570e835a873330853a
Mansehaj Singh <msehajs@gmail.com>
2023-06-06 18:45:31 -0600

    Removed service classes for new domains.

--- services/organization-manager/src/main/java/com/inmotionchat/organizations/service/contract/AclPermissionService.java
+++
@@ -1,9 +0,0 @@
-package com.inmotionchat.organizations.service.contract;
-
-import com.inmotionchat.core.domains.Role;
-
-public interface AclPermissionService {
-
-    void createPermissions(Role role);
-
-}
--- services/organization-manager/src/main/java/com/inmotionchat/organizations/service/contract/OrganizationService.java
+++
@@ -1,8 +0,0 @@
-package com.inmotionchat.organizations.service.contract;
-
-import com.inmotionchat.core.data.DomainService;
-import com.inmotionchat.core.data.dto.OrganizationDTO;
-import com.inmotionchat.core.domains.Organization;
-
-public interface OrganizationService extends DomainService<Organization, OrganizationDTO> {
-}
