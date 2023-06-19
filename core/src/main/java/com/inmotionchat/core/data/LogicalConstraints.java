f2f5ffbbb4574836e97a30039d313812ec965ab8
Mansehaj Singh <msehajs@gmail.com>
2023-06-19 17:39:20 -0600

    Got rid of old logical constraints.

--- core/src/main/java/com/inmotionchat/core/data/LogicalConstraints.java
+++ core/src/main/java/com/inmotionchat/core/data/LogicalConstraints.java
@@ -1,23 +1,4 @@
 package com.inmotionchat.core.data;
 
 public class LogicalConstraints {
-
-    public static class User {
-        public static final String UNIQUE_USERNAME = "UNIQUE_USERNAME";
-        public static final String UNIQUE_EMAIL_FOR_VERIFIED_USERS = "UNIQUE_EMAIL_FOR_VERIFIED_USERS";
-    }
-
-    public static class Session {
-        public static final String MAX_SESSIONS_REACHED = "MAX_SESSIONS_REACHED";
-    }
-
-    public static class Tenant {
-        public static final String CANNOT_CHANGE_TENANT = "CANNOT_CHANGE_TENANT";
-    }
-
-    public static class Role {
-        public static final String ONE_DEFAULT_ROLE = "ONE_DEFAULT_ROLE";
-        public static String ONE_ROOT_ROLE = "ONE_ROOT_ROLE";
-    }
-
 }
