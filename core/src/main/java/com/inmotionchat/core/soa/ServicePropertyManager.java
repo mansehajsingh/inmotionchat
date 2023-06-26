1dc9922fe6f43aee8cbd3cf92a5f964a171d516c
Mansehaj Singh <msehajs@gmail.com>
2023-06-25 18:04:39 -0600

    Added ServiceProperty dynamic support.

---
+++ core/src/main/java/com/inmotionchat/core/soa/ServicePropertyManager.java
@@ -0,0 +1,70 @@
+package com.inmotionchat.core.soa;
+
+import com.google.gson.internal.Primitives;
+import com.inmotionchat.core.exceptions.ServiceDeploymentException;
+import org.json.JSONException;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicePropertyManager {
+
+    private final JSONObject parsedConfigs;
+
+    private final InMotionService service;
+
+    public ServicePropertyManager(InMotionService inMotionService) throws IOException, ParseException {
+        String configDirectoryPath = System.getProperty("conf");
+        JSONParser parser = new JSONParser();
+        this.parsedConfigs = (JSONObject) parser.parse(
+                new FileReader(configDirectoryPath + "\\" + inMotionService.getServiceConfigFileName())
+        );
+        this.service = inMotionService;
+    }
+
+    public void fillServiceProperties() throws ServiceDeploymentException {
+
+        List<Method> setters = new ArrayList<>();
+
+        for(Method method : this.service.getClass().getDeclaredMethods()) {
+            if (method.isAnnotationPresent(ServiceProperty.class))
+                setters.add(method);
+        }
+
+        for (Method setter : setters) {
+            Class<?> configValueType = setter.getParameterTypes()[0];
+            ServiceProperty property = setter.getAnnotation(ServiceProperty.class);
+            Object configValue = "";
+
+            configValue = parsedConfigs.get(property.name());
+
+            if (configValue == null && property.required()) {
+                throw new ServiceDeploymentException(
+                        "Configuration property "
+                                + property.name()
+                                + " of service "
+                                + this.service.getServiceName()
+                                + " could not be resolved. Check "
+                                + this.service.getServiceConfigFileName()
+                                + " to ensure the property is present."
+                );
+            }
+
+            if (configValueType.isPrimitive()) {
+                configValueType = Primitives.wrap(configValueType);
+            }
+
+            try {
+                setter.invoke(this.service, configValueType.cast(configValue));
+            } catch(Exception e) {}
+        }
+
+    }
+
+}
--- services/identity-platform/src/main/java/com/inmotionchat/identity/IdentityPlatformService.java
+++ services/identity-platform/src/main/java/com/inmotionchat/identity/IdentityPlatformService.java
@@ -1,13 +1,21 @@
 package com.inmotionchat.identity;
 
+import com.inmotionchat.core.exceptions.ServiceDeploymentException;
 import com.inmotionchat.core.soa.InMotionService;
+import com.inmotionchat.core.soa.ServiceProperty;
+import com.inmotionchat.core.soa.ServicePropertyManager;
+import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
 public class IdentityPlatformService extends InMotionService {
     private static Logger log = LoggerFactory.getLogger(IdentityPlatformService.class);
+
+    private boolean useFirebaseEmulators = false;
 
     public IdentityPlatformService() {
         super(log);
@@ -15,6 +23,31 @@ public class IdentityPlatformService extends InMotionService {
 
     public String getServiceName() {
         return "IdentityPlatform";
+    }
+
+    @Override
+    public String getServiceConfigFileName() {
+        return "identity-platform.json";
+    }
+
+    @Override
+    public void awaken() throws IOException, ParseException, ServiceDeploymentException {
+        log.info("Starting " + getServiceName() + " service.");
+
+        ServicePropertyManager spm = new ServicePropertyManager(this);
+        spm.fillServiceProperties();
+
+        this.started = true;
+        log.info("Started " + getServiceName() + " service.");
+    }
+
+    @ServiceProperty(name = "useFirebaseEmulators", required = true)
+    public void setUseFirebaseEmulators(boolean useFirebaseEmulators) {
+        this.useFirebaseEmulators = useFirebaseEmulators;
+    }
+
+    public boolean useFireBaseEmulators() {
+        return this.useFirebaseEmulators;
     }
 
 }
