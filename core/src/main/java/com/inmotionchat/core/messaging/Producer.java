c9bf178d46f200ef6c0d315e26170f73712393ff
Mansehaj Singh <msehajs@gmail.com>
2023-07-02 10:43:37 -0600

    Added logging to producer class.

--- core/src/main/java/com/inmotionchat/core/messaging/Producer.java
+++ core/src/main/java/com/inmotionchat/core/messaging/Producer.java
@@ -1,6 +1,7 @@
 package com.inmotionchat.core.messaging;
 
 import com.inmotionchat.core.data.events.StreamEvent;
+import org.slf4j.Logger;
 import org.springframework.data.redis.connection.stream.ObjectRecord;
 import org.springframework.data.redis.connection.stream.StreamRecords;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -8,11 +9,14 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 public abstract class Producer<E extends StreamEvent<?>> {
 
+    protected final Logger log;
+
     protected final RedisTemplate<String, Object> redisTemplate;
 
     protected final String streamKey;
 
-    protected Producer(RedisTemplate<String, Object> redisTemplate, String streamKey) {
+    protected Producer(Logger log, RedisTemplate<String, Object> redisTemplate, String streamKey) {
+        this.log = log;
         this.redisTemplate = redisTemplate;
         this.streamKey = streamKey;
     }
@@ -26,6 +30,8 @@ public abstract class Producer<E extends StreamEvent<?>> {
         this.redisTemplate
                 .opsForStream()
                 .add(record);
+
+        log.debug("Produced event {}.", event);
     }
 
 }
