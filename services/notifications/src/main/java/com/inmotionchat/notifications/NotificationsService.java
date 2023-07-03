f26a9b4a11a55dd07311d932d981fa79f17f0b90
Mansehaj Singh <msehajs@gmail.com>
2023-07-03 13:46:45 -0600

    Added new service property for NotificationsService to accommodate active notifiers.

--- services/notifications/src/main/java/com/inmotionchat/notifications/NotificationsService.java
+++ services/notifications/src/main/java/com/inmotionchat/notifications/NotificationsService.java
@@ -1,14 +1,19 @@
 package com.inmotionchat.notifications;
 
 import com.inmotionchat.core.soa.InMotionService;
+import com.inmotionchat.core.soa.ServiceProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class NotificationsService extends InMotionService {
 
     protected final static Logger log = LoggerFactory.getLogger(NotificationsService.class);
+
+    private List<String> activeNotifierNames;
 
     public NotificationsService() {
         super(log);
@@ -22,6 +27,15 @@ public class NotificationsService extends InMotionService {
     @Override
     public String getServiceConfigFileName() {
         return "notifications.json";
+    }
+
+    @ServiceProperty(name = "activeNotifiers", required = true)
+    public void setActiveNotifierNames(List<String> activeNotifierNames) {
+        this.activeNotifierNames = activeNotifierNames;
+    }
+
+    public List<String> getActiveNotifierNames() {
+        return this.activeNotifierNames;
     }
 
 }
