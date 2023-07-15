9d7697df88b97448674fcfec6de6dd63e18b3a3c
Mansehaj Singh <msehajs@gmail.com>
2023-07-15 16:32:43 -0600

    Added workflow domain and dto.

---
+++ core/src/main/java/com/inmotionchat/core/data/postgres/workflow/Workflow.java
@@ -0,0 +1,62 @@
+package com.inmotionchat.core.data.postgres.workflow;
+
+import com.inmotionchat.core.data.Schema;
+import com.inmotionchat.core.data.dto.WorkflowDTO;
+import com.inmotionchat.core.data.postgres.AbstractArchivableDomain;
+import com.inmotionchat.core.data.postgres.identity.Tenant;
+import com.inmotionchat.core.exceptions.DomainInvalidException;
+import com.inmotionchat.core.util.validation.AbstractRule;
+import com.inmotionchat.core.util.validation.StringRule;
+import com.inmotionchat.core.util.validation.Violation;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "workflows", schema = Schema.WorkflowManagement)
+public class Workflow extends AbstractArchivableDomain<Workflow> {
+
+    private String name;
+
+    @ManyToOne
+    private Tenant tenant;
+
+    public Workflow() {}
+
+    public Workflow(String name, Tenant tenant) {
+        this.name = name;
+        this.tenant = tenant;
+    }
+
+    public Workflow(Long tenantId, WorkflowDTO proto) {
+        this.tenant = new Tenant(tenantId);
+        this.name = proto.name();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Tenant getTenant() {
+        return this.tenant;
+    }
+
+    @Override
+    public void validate() throws DomainInvalidException {
+        AbstractRule<String> nameRule = StringRule.forField("name").isNotNull().isNotEmpty();
+
+        List<Violation> violations = new ArrayList<>(nameRule.collectViolations(this.name));
+
+        if (!violations.isEmpty())
+            throw new DomainInvalidException(violations);
+    }
+
+}
