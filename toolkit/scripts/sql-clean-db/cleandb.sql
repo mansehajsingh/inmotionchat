6c25e31a8c6d7fdf5a9ede0735e25042e13ee313
Mansehaj Singh <msehajs@gmail.com>
2023-07-14 21:26:48 -0600

    Added script for cleaning database tables.

---
+++ toolkit/scripts/sql-clean-db/cleandb.sql
@@ -0,0 +1,9 @@
+DELETE FROM identityplatform.permissions;
+DELETE FROM identityplatform.resolution_domains;
+DELETE FROM identityplatform.role_assignments;
+DELETE FROM identityplatform.roles;
+DELETE FROM inboxmanagement.inbox_group_assignments;
+DELETE FROM inboxmanagement.inbox_groups;
+DELETE FROM inboxmanagement.inboxes;
+DELETE FROM identityplatform.users;
+DELETE FROM identityplatform.tenants;
