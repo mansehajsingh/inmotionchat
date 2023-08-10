08d3a3d458573926bcb06da9f2546526042e686d
Mansehaj Singh <msehajs@gmail.com>
2023-08-10 16:11:08 -0600

    Failed authentication response has correct content type and sends message depending on type of failure.

--- core/src/main/java/com/inmotionchat/core/web/AuthenticationError.java
+++ core/src/main/java/com/inmotionchat/core/web/AuthenticationError.java
@@ -1,7 +1,7 @@
 package com.inmotionchat.core.web;
 
 public class AuthenticationError {
-    public static final String ROLE_INCURRED_CHANGE = "ROLE_INCURRED_CHANGE";
     public static final String NO_BEARER_PROVIDED = "NO_BEARER_PROVIDED";
     public static final String INVALID_TOKEN = "INVALID_TOKEN";
+    public static final String EXPIRED_TOKEN = "EXPIRED_TOKEN";
 }
