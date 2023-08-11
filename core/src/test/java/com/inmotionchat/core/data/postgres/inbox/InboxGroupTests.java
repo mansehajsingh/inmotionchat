a5c91a14408d5586c42a71ca351f8c3b50a427e2
Mansehaj Singh <msehajs@gmail.com>
2023-08-10 23:09:08 -0600

    Updated all the pom files to use revision property for version number. Also fixed test that was failing build.

--- core/src/test/java/com/inmotionchat/core/data/postgres/inbox/InboxGroupTests.java
+++ core/src/test/java/com/inmotionchat/core/data/postgres/inbox/InboxGroupTests.java
@@ -4,27 +4,29 @@ import com.inmotionchat.core.data.dto.InboxGroupDTO;
 import com.inmotionchat.core.exceptions.DomainInvalidException;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class InboxGroupTests {
 
     private static final String validName = "Ibx Group";
 
     @Test
     public void validateInboxGroup_Success() throws DomainInvalidException {
-        InboxGroupDTO inboxGroupDTO = new InboxGroupDTO(validName);
+        InboxGroupDTO inboxGroupDTO = new InboxGroupDTO(validName, new ArrayList<>());
         InboxGroup group = new InboxGroup(5L, inboxGroupDTO);
         group.validate();
     }
 
     @Test(expected = DomainInvalidException.class)
     public void validateInboxGroup_NullName() throws DomainInvalidException {
-        InboxGroupDTO inboxGroupDTO = new InboxGroupDTO(null);
+        InboxGroupDTO inboxGroupDTO = new InboxGroupDTO(null, new ArrayList<>());
         InboxGroup group = new InboxGroup(5L, inboxGroupDTO);
         group.validate();
     }
 
     @Test(expected = DomainInvalidException.class)
     public void validateInboxGroup_EmptyName() throws DomainInvalidException {
-        InboxGroupDTO inboxGroupDTO = new InboxGroupDTO("");
+        InboxGroupDTO inboxGroupDTO = new InboxGroupDTO("", new ArrayList<>());
         InboxGroup group = new InboxGroup(5L, inboxGroupDTO);
         group.validate();
     }
