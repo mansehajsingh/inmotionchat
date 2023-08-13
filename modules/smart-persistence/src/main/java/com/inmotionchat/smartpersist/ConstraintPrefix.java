185a14fb9e5fbb19a61d701ad3ab8e0fe979511e
Mansehaj Singh <msehajs@gmail.com>
2023-08-12 21:06:08 -0600

    Added querying module for better jpa repository persistence apis.

---
+++ modules/smart-persistence/src/main/java/com/inmotionchat/smartpersist/ConstraintPrefix.java
@@ -0,0 +1,9 @@
+package com.inmotionchat.smartpersist;
+
+public class ConstraintPrefix {
+
+    public static final String UNIQUE = "UNIQUE_";
+
+    public static final String FKEY = "FKEY_";
+
+}
