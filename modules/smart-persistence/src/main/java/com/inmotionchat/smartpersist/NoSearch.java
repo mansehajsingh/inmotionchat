1fd207058e981f78d8c533649e25a7232b12a0b2
Mansehaj Singh <msehajs@gmail.com>
2023-08-15 22:55:09 -0600

    Added ability to filter out searching on certain columns.

---
+++ modules/smart-persistence/src/main/java/com/inmotionchat/smartpersist/NoSearch.java
@@ -0,0 +1,8 @@
+package com.inmotionchat.smartpersist;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NoSearch {
+}
