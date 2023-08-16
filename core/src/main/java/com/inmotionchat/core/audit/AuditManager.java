4e32a022d462a3a2194487db9fcec3a5f57b83fa
Mansehaj Singh <msehajs@gmail.com>
2023-08-15 22:07:38 -0600

    Retrieval of audit records is complete.

--- core/src/main/java/com/inmotionchat/core/audit/AuditManager.java
+++ core/src/main/java/com/inmotionchat/core/audit/AuditManager.java
@@ -1,7 +1,13 @@
 package com.inmotionchat.core.audit;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.util.MultiValueMap;
+
 public interface AuditManager {
 
     AuditLog save(AuditLog log);
+
+    Page<AuditLog> search(Long tenantId, Pageable pageable, MultiValueMap<String, Object> parameters);
 
 }
